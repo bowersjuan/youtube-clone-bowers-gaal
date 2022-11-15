2993087cb059fd09e99f5d43f0e053c2bea0092b
bowersjuan <bowersjuan@gmail.com>
2022-11-15 15:04:10 -0500

    Start setting up fetch call for search click

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,26 +1,34 @@
 import { useState } from "react";
 import "./Main.css";
 
-// const BASE_URL = process.env.BASE_URL;
-// const reactDevYoutubeAPI = process.env.REACT_APP_YOUTUBE_API;
-
-// let queryParam1 = "snippet"
-// ?"part=snippet"&
+const BASE_URL =
+  "https://youtube.googleapis.com/youtube/v3/search?part=snippet&key=";
+const reactDevYoutubeAPI = process.env.REACT_APP_YOUTUBE_API;
 
 const Main = () => {
+  const [videos, setVideos] = useState([]);
   const [searchBox, setSearchBox] = useState("");
 
   const handleTextChange = (e) => {
     setSearchBox(e.target.value);
   };
 
-  const handleClick = () => {
+  const handleClick = (e) => {
+    e.preventDefault();
+    fetch(`${BASE_URL}${reactDevYoutubeAPI}&type=video&q=avicii`)
+      .then((res) => res.json())
+      .then((res) => {
+        setVideos(res);
+      })
+      .catch((error) => console.log(error));
+
+    console.log(videos);
     // window.localStorage.setItem("searchBox", JSON.stringify(searchBox));
   };
 
   return (
     <div>
-      <form>
+      <form onSubmit={handleClick}>
         <label htmlFor="search"></label>
         <input
           className="searchBox"
@@ -30,9 +38,7 @@ const Main = () => {
           placeholder="Search..."
           type="text"
         ></input>
-        <button onClick={handleClick} type="submit">
-          Search
-        </button>
+        <button type="submit">Search</button>
       </form>
     </div>
   );
