c9c2f74d97991b572ad37624c14e1a715b48d807
bowersjuan <bowersjuan@gmail.com>
2022-11-15 12:11:27 -0500

    Update Navbar.css

--- src/common/Navbar.js
+++ src/common/Navbar.js
@@ -4,7 +4,7 @@ import "./Navbar.css";
 export default function Navbar() {
   return (
     <div className="Navbar">
-      <h1 className="Header">YouTube Clone</h1>
+      <h1 className="header">YouTube Clone</h1>
       <Link to="/">Home</Link> <Link to="/about">About</Link>
     </div>
   );
