deb0fba1361bfe89ddb6fa66586ca7cb5c6900ce
bowersjuan <bowersjuan@gmail.com>
2022-11-17 17:21:05 -0500

    Create and Update NoSearch modal component

---
+++ src/components/NoSearch.js
@@ -0,0 +1,13 @@
+const NoSearch = () => {
+  return (
+    <>
+      <button>Click for Modal</button>
+      <div id="modal" className="w3-modal">
+        <div className="w3-modal-content"></div>
+        <h1> MODAL </h1>
+      </div>
+    </>
+  );
+};
+
+export default NoSearch;
