a1392f8e02dec21b24b6c2e80a32b859453f94c3
bowersjuan <bowersjuan@gmail.com>
2022-11-25 19:36:28 -0500

    Update url.js into fetch.js with async await, condense query parameters and update Main.js form

--- src/API/url.js
+++
@@ -1,6 +0,0 @@
-const BASE_URL = "https://youtube.googleapis.com/youtube/v3/search?";
-const urlSnippet = `part=snippet`;
-const urlTypeVideo = `type=video&q=`;
-const urlKey = `key=${process.env.REACT_APP_YOUTUBE_API}`;
-
-export { BASE_URL, urlSnippet, urlTypeVideo, urlKey };
