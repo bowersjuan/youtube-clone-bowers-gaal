211f6b2bc680c765af8b98c6be3ae0d31ba38a92
Jonathan-Gaal <jonathangaal@pursuit.org>
2022-11-28 14:14:04 -0500

    getting comments rendering but still not updating on submit

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,48 +1,75 @@
-// import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import YouTube from "react-youtube";
 import "./Video.css";
-// import {
-//   BASE_URL,
-//   reactDevYoutubeAPI,
-//   queryParam1,
-//   queryParam2,
-//   queryParam3,
-// } from "../API/url";
 
 const Video = () => {
   const { id } = useParams();
-  // const queryParam3 = `id=${id}`;
-  // const SINGLEVID_URL = ``;
 
-  // useEffect(() => {
-  //   fetch()
-  //     .then()
-  //     .then()
-  //     .catch((err) => {
-  //       console.error(err);
-  //     });
-  // }, []);
+  const [comments, setComments] = useState([]);
 
-  // const [commentsInput, setCommentsInput] = useState({
-  //   commenter: "",
-  //   comment: "",
-  // });
+  const [commentInput, setCommentInput] = useState({
+    commenter: "",
+    comment: "",
+  });
+
+  const handleTextChange = (e) => {
+    setCommentInput({ ...commentInput, [e.target.id]: e.target.value });
+  };
+
+  const commentsToStorage = (comments) =>
+    window.localStorage.setItem(id, JSON.stringify(comments));
+
+  const commentsFromStorage = JSON.parse(window.localStorage.getItem(id));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setComments([...comments, commentInput]);
+    setCommentInput({
+      commenter: "",
+      comment: "",
+    });
+  };
+
+  useEffect(() => {
+    commentsToStorage(comments);
+  }, [comments]);
 
   return (
     <div className="video">
       <YouTube videoId={id} />;
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="commenter">
-          Name: <input type="text" id="commenter"></input>
+          Name:{" "}
+          <input
+            type="text"
+            id="commenter"
+            value={commentInput.commenter}
+            onChange={handleTextChange}></input>
         </label>
         <label htmlFor="comment">
           {" "}
-          Comment: <input type="text" id="comment"></input>{" "}
+          Comment:{" "}
+          <input
+            type="text"
+            id="comment"
+            value={commentInput.comment}
+            onChange={handleTextChange}></input>{" "}
         </label>
         <button type="submit">Add Comment</button>
         <ul id="commentsUl"></ul>
       </form>
+      <ul>
+        {commentsFromStorage?.map((comment, indexId) => {
+          return (
+            <li id={indexId}>
+              <p>
+                {comment.commenter} - {comment.comment}
+              </p>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
